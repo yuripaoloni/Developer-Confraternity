7f637b20b49cd18d1dd15ef680956f4ad0acfe67
Matteo Leonesi <38867931+Msnaps@users.noreply.github.com>
2020-01-27 17:41:47 +0100

    created User schema

---
+++ models/User.js
@@ -0,0 +1,27 @@
+//schema per db
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @des     Register user
 // @access  Public
-router.get("/", (req, res) => res.send("user route"));
+router.post(
+  "/",
+  [
+    check("name", "name is required")
+      .not()
+      .isEmpty(),
+    check("email", "please include a valid email").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("user route");
+  }
+);
 
 module.exports = router;
