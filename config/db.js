cc27c2132677c791ddc9a792324bd2419483efe2
Yuri Paoloni <yuri.paoloni3@gmail.com>
2020-01-29 17:24:13 +0100

    added now connections properties

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useFindAndModify: false
     });
     console.log("MongoDB connected...");
   } catch (err) {
