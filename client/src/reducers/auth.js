de38ee82c408fd33676e34122962c7bdea6d9c1b
Matteo Leonesi <38867931+Msnaps@users.noreply.github.com>
2020-02-06 18:09:53 +0100

    implemented Logout

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from "../actions/types";
 import { STATES } from "mongoose";
 
@@ -39,6 +40,7 @@ export default function(state = initialState, actions) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem("token");
       return {
         ...state,
