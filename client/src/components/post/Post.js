36e9e9eb32101f9b9f09ddeb78ff1460b09cf8e0
Yuri Paoloni <yuri.paoloni3@gmail.com>
2020-02-13 18:43:30 +0100

    adding & display comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
+import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 import { getPost } from "../../actions/post";
-import { Link } from "react-router-dom";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -18,6 +20,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back to posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
